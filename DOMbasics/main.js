5bb80ba69bcf09f8df7ceb52ae1e91649bd571b0
nabendu82 <nabendu.biswas@gmail.com>
2022-06-13 22:20:00 +0530

    DOM Basics

--- DOMbasics/main.js
+++ DOMbasics/main.js
@@ -7,13 +7,51 @@
 // console.log(document.querySelector('#my-form'));
 
 //Select multiple elements
-const nodeItems = document.querySelectorAll('.item');
-const colItems = document.getElementsByClassName('item');
+// const nodeItems = document.querySelectorAll('.item');
+// const colItems = document.getElementsByClassName('item');
 // console.log(document.getElementsByTagName('li'));
 // console.log([34, 33]);
 
 // nodeItems.forEach(item => console.log(item));
 // colItems.forEach(item => console.log(item));
-for(let i = 0; i < colItems.length; i++){
-    console.log(colItems[i]);
+// for(let i = 0; i < colItems.length; i++){
+//     console.log(colItems[i]);
+// }
+
+//DOM Manipulation
+// const ul = document.querySelector('.items');
+// ul.remove();
+
+//Event Listener
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', e => {
+//     e.preventDefault();
+//     ul.firstElementChild.textContent = 'HTML';
+//     ul.children[1].innerText = 'CSS';
+//     ul.lastElementChild.innerHTML = '<h1>JavaScript</h1>';
+// })
+
+//Form manipulation
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerText = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+        userList.appendChild(li);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
 }
